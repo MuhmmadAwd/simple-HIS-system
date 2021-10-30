27a6097a277a987c8152e226e42950d472ebd6b6
mohmed2000 <noza25588@gmail.com>
2021-10-30 08:23:23 +0200

    15) Data-Service :: Create Data Class

--- app/patient/patient-edit.js
+++ app/patient/patient-edit.js
@@ -4,19 +4,14 @@ class PatientEdit {
     this.formMode = "";
   }
   init = () => {
-    this.initEvent();
+    dataService.initEvent();
   };
-  initEvent() {
-    $(".patient-edit .patient-save").click(this.onSaveClick);
-    $(".patient-edit .patient-warning").click(this.onBeforeDeleteClick);
-    $(".patient-delete").click(this.onDeleteClick);
-  }
 
   open(patientID) {
     this.patientID = patientID;
     if (patientID) {
       this.formMode = "Edit";
-      let patientObj = this.getById(patientID);
+      let patientObj = dataService.getById(patientID);
       this.LoadControlData(patientObj);
     } else {
       this.formMode = "New";
@@ -61,65 +56,6 @@ class PatientEdit {
     $(".date-input").val("1954-02-09");
     $(`.active-input[value = ${patientObj.Active}]`).attr("checked", "checked");
     $(`.form-select option[value = 1]`).attr("selected", "selected");
-  };
-
-  // -----------------------------------------------------------------
-  // ******** data functions ************
-  // -----------------------------------------------------------------
-
-  onSaveClick = () => {
-    let data = this.GetControlsData();
-    if (this.formMode == "Edit") {
-      let currentID = data.ID;
-      let index = this.getIndexById(currentID);
-      patientsData[index].fname = data.fname;
-      patientsData[index].mname = data.mname;
-      patientsData[index].lname = data.lname;
-      patientsData[index].email = data.email;
-      patientsData[index].DOB = data.DOB;
-      patientsData[index].gender = data.gender;
-      patientsData[index].Active = data.Active;
-    } else if (this.formMode == "New") {
-      data.ID = this.getNewID();
-      let templateText = $("#patient-list-template").html();
-      let RendertemplateFun = templateEngine.Rendertemplate(templateText, data);
-      $(".patient-table-data").append(RendertemplateFun);
-    }
-    router.navigate(".patient-list");
-  };
-
-  onBeforeDeleteClick() {
-    $(".warning-alert").show();
-  }
-
-  getById = (patientID) => {
-    for (let i = 0; i < patientsData.length; i++) {
-      if (patientsData[i].ID == patientID) {
-        return patientsData[i];
-      }
-    }
-  };
-  getIndexById = (patientID) => {
-    for (let i = 0; i < patientsData.length; i++) {
-      if (patientsData[i].ID == patientID) {
-        return i;
-      }
-    }
-  };
-  getNewID = (patientID) => {
-    for (let i = 0; i < patientsData.length; i++) {
-      if (patientsData[i].ID == patientID) {
-        return patientsData[patientsData.length].ID;
-      }
-    }
-  };
-  onDeleteClick = () => {
-    $(".warning-alert").hide();
-    let patientID = this.patientID;
-    let index = this.getIndexById(patientID);
-    let patientObj = patientsData[index];
-    patientsData.splice(index, 1);
-    return patientObj;
   };
 
   resetControls() {
